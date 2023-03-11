cf501d28c905fd553610aaa2207248185afb2308
loboph <lobophf@gmail.com>
2023-03-11 17:25:31 -0300

    fixing mistake

--- product-service/src/main/java/dev/pedrolobo/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/dev/pedrolobo/productservice/ProductServiceApplication.java
@@ -6,18 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = "/")
 @SpringBootApplication
 public class ProductServiceApplication {
 
-  @GetMapping()
-  public String ss(){
-    return "<h1 style=color:deepPink>QUERO IR EMBORAAAA!</h1>";
-  }
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
-    System.out.println("aaaaaa");
 	}
 
 }
